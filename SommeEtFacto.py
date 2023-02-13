c6ec4922016591e20c5cd8fe5be0bbbb65c2385b
alixdunet <alix.techoffice@gmail.com>
2023-02-13 19:06:32 +0100

    Nettoyage

--- SommeEtFacto.py
+++
@@ -1,6 +0,0 @@
-def somme_et_facto():
-    
-    A = int(input("Entrer un entier positif : "))
-
-
-somme_et_facto()
--- notes.md
+++
@@ -1,5 +0,0 @@
-# Algorithme
-
-entrer un entier positif : 5
-
-1 + 2 + 3 + 4 + 5 = 15
